8d4b27123a1b7927f042a81fae249fb22e1c5be6
Zaid Ali Ajo <zaidejjo@gmail.com>
2025-11-03 01:16:23 +0300

    3/11/2025

--- school_portal/ai_chat/models.py
+++ school_portal/ai_chat/models.py
@@ -1,4 +1,3 @@
-# models.py
 from django.db import models
 
 class FAQ(models.Model):
@@ -7,3 +6,8 @@ class FAQ(models.Model):
 
     def __str__(self):
         return self.question
+
+class ChatHistory(models.Model):
+    message = models.TextField()
+    sender = models.CharField(max_length=10)  # 'user' أو 'ai'
+    timestamp = models.DateTimeField(auto_now_add=True)
