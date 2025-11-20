7173cba1b06e15210d72fc4e63ec371b24da7bb0
Zaid Ajo <zaidejjo@gmail.com>
2025-11-20 21:04:18 +0300

    add xml sitemape.py files1

--- school_portal/ai_chat/sitemaps.py
+++ school_portal/ai_chat/sitemaps.py
@@ -6,7 +6,7 @@ class AIChatSitemap(Sitemap):
     priority = 0.5
 
     def items(self):
-        return ['chat:home']
+        return ['chat_home', 'chat_ai']  # أسماء الـ views فقط
 
     def location(self, item):
-        return reverse(item)
+        return reverse(f'ai_chat:{item}')  # استخدام namespace
